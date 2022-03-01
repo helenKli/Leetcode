ffb9ca5860be6533472fe22f54b1de9c5b9f5a09
Helen Kli <klitsunova2000@mail.ru>
2022-03-01 16:35:31 +0300

    Add 58th problem

---
+++ 58. Length of Last Word/code.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/length-of-last-word/
+
+#include <iostream>
+#include <string>
+
+int lengthOfLastWord(const std::string s) {
+    int l = 0;
+    int end = s.length() - 1;
+    while (s[end] == ' ') {
+        end--;
+    }
+    while (s[end] != ' ') {
+        l++;
+        end--;
+        if (end == -1) {
+            break;
+        }
+    }
+    return l;
+}
+
+void test(const std::string& str, const int& correctAnswer) {
+    const int result = lengthOfLastWord(str);
+    if (result == correctAnswer) {
+        std::cout << "Test passed" << std::endl;
+    } else {
+        std::cout << "Test not passed" << std::endl;
+    }
+}
+
+int main() {
+    test("luffy is still joyboy", 6);
+    test("   fly me   to   the moon  ", 4);
+}
+
