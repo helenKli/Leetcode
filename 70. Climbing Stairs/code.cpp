7d54195b8c5292b844ac4858742e89d9ff8b96ae
Helen Kli <klitsunova2000@mail.ru>
2022-03-02 17:34:46 +0300

    Add 70th problem

---
+++ 70. Climbing Stairs/code.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/climbing-stairs/
+
+#include <iostream>
+
+int climbStairs(const int& n) {
+    if (n == 1) {
+        return n;
+    }
+    int count[n];
+    count[0] = 1;
+    count[1] = 2;
+    for (int i = 2; i < n; i++) {
+        count[i] = count[i - 1] + count[i - 2];
+    }
+    return count[n - 1];
+}
+
+void test(const int& val, const int& correctAnswer) {
+    int result = climbStairs(val);
+    if (result == correctAnswer) {
+        std::cout << "Test passed" << std::endl;
+    } else {
+        std::cout << "Test not passed" << std::endl;
+    }
+}
+
+int main() {
+    test(10, 89);
+    test(25, 121393);
+}
